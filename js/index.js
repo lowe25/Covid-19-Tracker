83c51c346eabb72105d8e25ec064fb5de0a25698
Louie Cabral <louiealdrin25@gmail.com>
2021-09-22 16:39:53 +0800

    Table Country Fixed

--- js/index.js
+++ js/index.js
@@ -18,20 +18,21 @@ var GlobalconfirmedtotalDeaths = document.getElementById("total-deaths");
 var GlobalconfirmednewDeaths = document.getElementById("new-deaths");
 
 //Country Box
+//var txtSearch = document.getElementById("country-src").value;
 var Countryconfirmed = document.getElementById("selected-confirmed");
 var CountryconfirmednewCase = document.getElementById("selected-new-case");
 var CountryconfirmedtotalCase = document.getElementById("selected-total-case");
 var Countryconfirmedrecoveries = document.getElementById("selected-recoveries");
-var CountryconfirmedtotalDeaths = document.getElementById("selected-total-deaths");
+var CountryconfirmedtotalDeaths = document.getElementById(
+  "selected-total-deaths"
+);
 var CountryconfirmednewDeaths = document.getElementById("selected-new-deaths");
-
-var txtSearch = document.getElementById("country-src").value;
+var txtSearch ="Canada";
 const apiPostTbl = "https://covid-193.p.rapidapi.com/statistics";
-const apiPostSearch =
-  `https://covid-193.p.rapidapi.com/statistics?country=${txtSearch}`;
+const apiPostSearch = `https://covid-193.p.rapidapi.com/statistics?country=${txtSearch}`;
 const globalStatus = "https://covid-193.p.rapidapi.com/statistics?country=all";
-
-
+const table = document.querySelector(".betlog");
+var row;
 function tblFill() {
   fetch(apiPostTbl, {
     method: "GET",
@@ -46,23 +47,37 @@ function tblFill() {
       }
     })
     .then((response) => {
-     // console.log(response);
-        for (const d of response.response) {
-      country.innerHTML = `${d.country}`;
-      totalCase.innerHTML = `${d.cases.total}`;
-      newCase.innerHTML = `${d.cases.new}`;
-      totalDeaths.innerHTML = `${d.deaths.total}`;
-      newDeaths.innerHTML = `${d.deaths.new}`;
-      totalReco.innerHTML = `${d.cases.total}`;
-      newReco.innerHTML = `${d.cases.new}`;
-    }
+      // console.log(response);
+      /*       
+      console.log(response)
+        country.innerHTML = `${d.country}`;
+        totalCase.innerHTML = `${d.cases.total}`;
+        newCase.innerHTML = `${d.cases.new}`;
+        totalDeaths.innerHTML = `${d.deaths.total}`;
+        newDeaths.innerHTML = `${d.deaths.new}`;
+        totalReco.innerHTML = `${d.cases.total}`;
+        newReco.innerHTML = `${d.cases.new}`;
+      */
+      for (const d of response.response) {
+        console.log(response)
+       row = `<tr>
+              <td>${d.country}</td>
+              <td>${d.cases.total}</td>
+              <td>${d.cases.new}</td>
+              <td>${d.deaths.total}</td>
+              <td>${d.deaths.new}</td>
+              <td>${d.cases.recovered}</td>
+              </tr>
+        `
+        table.innerHTML += row;
+      }
     })
     .catch((err) => {
       console.error(err);
     });
-  
 }
 
+//Search Country
 function searchCountryFetch() {
   fetch(apiPostSearch, {
     method: "GET",
@@ -91,7 +106,7 @@ function searchCountryFetch() {
       console.error(err);
     });
 }
-
+//Global Status Box
 function globalStatusFunc() {
   fetch(globalStatus, {
     method: "GET",
@@ -106,7 +121,7 @@ function globalStatusFunc() {
       }
     })
     .then((response) => {
-     // console.log(response);
+      // console.log(response);
       for (const d of response.response) {
         Globalconfirmed.innerHTML = `${d.cases.active}`;
         GlobalconfirmednewCase.innerHTML = `${d.cases.new}`;
@@ -121,6 +136,6 @@ function globalStatusFunc() {
     });
 }
 
-searchCountryFetch();
 tblFill();
+searchCountryFetch();
 globalStatusFunc();
