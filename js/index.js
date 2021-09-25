737ec443646eebb069be2e18014a8b961320c359
Louie Cabral <louiealdrin25@gmail.com>
2021-09-25 17:02:29 +0800

    FINAL COMMIT

--- js/index.js
+++ js/index.js
@@ -17,19 +17,6 @@ var Globalconfirmedrecoveries = document.getElementById("recoveries");
 var GlobalconfirmedtotalDeaths = document.getElementById("total-deaths");
 var GlobalconfirmednewDeaths = document.getElementById("new-deaths");
 
-//Country Box
-var txtSearch = document.getElementById("country-src").value;
-var Countryconfirmed = document.getElementById("selected-confirmed");
-var CountryconfirmednewCase = document.getElementById("selected-new-case");
-var CountryconfirmedtotalCase = document.getElementById("selected-total-case");
-var Countryconfirmedrecoveries = document.getElementById("selected-recoveries");
-var CountryconfirmedtotalDeaths = document.getElementById(
-  "selected-total-deaths"
-);
-var CountryconfirmednewDeaths = document.getElementById("selected-new-deaths");
-var btn = document.getElementById("btn-search");
-btn.addEventListener("click", getCountry);
-btn.addEventListener("click", refreshPage);
 /*
 second api
 		"x-rapidapi-host": "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
@@ -37,43 +24,8 @@ second api
 const apiPostTbl = "https://covid-193.p.rapidapi.com/statistics";
 const globalStatus = "https://covid-193.p.rapidapi.com/statistics?country=all";
 const table = document.querySelector(".tbl-list");
-var row;
 
-async function getCountry(){
-  const apiPostSearch = `https://covid-193.p.rapidapi.com/statistics?country=${txtSearch}`;
-  try{
-    const response = await fetch(apiPostSearch,{
-      method: "GET",
-      headers: {
-        "x-rapidapi-host": "covid-193.p.rapidapi.com",
-        "x-rapidapi-key": "568cd133bbmshb727f89d2da78d6p192e4djsnc6938c201e7d"
-      }
-    })
-    if(response.ok){
-      const jsonResponse = await response.json();
-      printCountry(jsonResponse);
-      return response;
-    }
-  }catch(error){
-    console.log(error);
-  }
-}
 
-function refreshPage(){
-  window.location.reload();
-}
-
-function printCountry(response){
-  for (const d of response.response) {
-    console.log(response)
-    Countryconfirmed.innerHTML = `${d.cases.active}`;
-    CountryconfirmednewCase.innerHTML = `${d.cases.new}`;
-    CountryconfirmedtotalCase.innerHTML = `${d.cases.total}`;
-    Countryconfirmedrecoveries.innerHTML = `${d.cases.recovered}`;
-    CountryconfirmedtotalDeaths.innerHTML = `${d.deaths.total}`;
-    CountryconfirmednewDeaths.innerHTML = `${d.deaths.new}`;
-  }
-}
 
 async function getglobalStatus() {
   try {
@@ -154,7 +106,6 @@ function printCountryTbl(response) {
           </tr>
     `;
     table.innerHTML += row;
-    sortTable(response);
   }
 }
 //FOR Tables 
@@ -195,12 +146,6 @@ function sortCountry(n) {
     }
   }
 }
-
-//Sort Numbers
-function sortTable(n) {
-
-}
-getCountry();
 getglobalStatus();
 getCountryTbl();
 
